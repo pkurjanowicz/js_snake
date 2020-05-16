f73cfcd6ef10b91c14969b0565ab592b75724184
peter kurjanowicz <pkurjanowicz5@gmail.com>
2020-05-16 10:49:34 -0600

    completed snake eating

--- draw.js
+++ draw.js
@@ -8,11 +8,20 @@ var snake;
 
 (function setup(){
   snake = new Snake();
+  fruit = new Fruit();
+
+  fruit.pickLocation()
 
   window.setInterval(() => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    snake.update()
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    fruit.draw();
+    snake.update();
     snake.draw(); 
+
+    if (snake.eat(fruit)) {
+      fruit.pickLocation();
+      
+    }
   }, 250);
 }());
 
--- snake.js
+++ snake.js
@@ -3,13 +3,26 @@ function Snake() {
   this.y = 0;
   this.xSpeed = scale * 1;
   this.ySpeed = 0;
+  this.total = 0;
+  this.tail = [];
 
   this.draw = function() {
     ctx.fillStyle = "#FFFFFF";
+
+    for (let i = 0; i < this.tail.length; i++) {
+      ctx.fillRect(this.tail[i].x, this.tail[i].y, scale, scale);
+    }
+
     ctx.fillRect(this.x, this.y, scale, scale);
   }
 
   this.update = function() {
+    for (let i = 0; i < this.tail.length - 1; i++ ) {
+      this.tail[i] = this.tail[i+1]
+    }
+
+    this.tail[this.total - 1]= {x: this.x, y: this.y};
+
     this.x += this.xSpeed;
     this.y += this.ySpeed;
 
@@ -49,5 +62,13 @@ function Snake() {
         this.ySpeed = 0;  
       break; 
     }
+  }
+
+  this.eat = function(fruit) {
+    if (this.x === fruit.x && this.y === fruit.y) {
+      this.total++;
+      return true;
+    }
+    return false;
   }
 }
